700597ea8d00e659baac69341f409fe3b3f4b42a
BrandonCate95 <brandoncate95@gmail.com>
2026-09-09 13:59:36 -0400

    cleanup slop tests

--- pkg/sqlstreams/export_test.go
+++
@@ -1,9 +0,0 @@
-package sqlstreams
-
-import "github.com/agentstax/sqlstreams/pkg/datastore"
-
-// The external test package reads what the client captured at construction.
-
-func (c *Client) Datastore() *datastore.PostgresDatastore { return c.ds }
-
-func (c *Client) ManagerDisabled() bool { return c.disableManager }
