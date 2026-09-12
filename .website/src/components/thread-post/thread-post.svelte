1e925f3972c79b68bc5283904bbd6b9a5f507678
BrandonCate95 <brandoncate95@gmail.com>
2026-09-12 08:31:53 -0400

    optimize profile pic

--- .website/src/components/thread-post/thread-post.svelte
+++ .website/src/components/thread-post/thread-post.svelte
@@ -48,7 +48,7 @@
 	</span>
 	<span class="author-role">{role}</span>
 	<a class="avatar" href={`/members/${author}/`}
-		><img src="/cat.png" width="44" height="44" alt={`${author} profile`} /></a
+		><img src="/cat-88.webp" width="44" height="44" alt={`${author} profile`} /></a
 	>
 	{#if postCount !== null}
 		<span class="post-count">Posts: {postCount}</span>
