9a518497816b029a215d840dc20c949657fa22b8
BrandonCate95 <brandoncate95@gmail.com>
2026-09-12 08:28:30 -0400

    header homepage link

--- .website/src/components/board-banner/board-banner.svelte
+++ .website/src/components/board-banner/board-banner.svelte
@@ -1,6 +1,8 @@
 <header class="board-banner">
 	<div class="identity">
-		<img class="wordmark" src="/sqlstreams-dark.svg" alt="SQLStreams" width="324" height="60" />
+		<a class="home-link" href="/" aria-label="SQLStreams home">
+			<img class="wordmark" src="/sqlstreams-dark.svg" alt="SQLStreams" width="324" height="60" />
+		</a>
 		<p class="tagline">the messaging platform that is just Postgres</p>
 	</div>
 </header>
